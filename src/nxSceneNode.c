494a9ecdf1c212aa593671b702ece1a6097a9ad9
Jonathan Frawley <jonathanfrawley@gmail.com>
2011-11-21 05:38:39 +0100

    Adding missing file

---
+++ src/nxSceneNode.c
@@ -0,0 +1,25 @@
+#include "nxSceneNode.h"
+
+void nxSceneNode_init(nxSceneNode* obj)
+{
+	obj->id = 0;
+	obj->valid = 0;
+	obj->hasTex = 0;
+	obj->type = 0;
+	nxVector2_init(&obj->pos);
+	obj->rot = 0;
+	obj->moving = 0;
+	obj->animTime = 0;
+	obj->animFrameTime = 0;
+	obj->maxAnimTime = 0;
+	obj->texId = 0;
+	obj->standingTexId = 0;
+	obj->crouchingTexId = 0;
+    for(int i=0;i<NX_MAX_ANIMTEXS;i++)
+    {
+        obj->animTexIds[i] = 0;
+    }
+	obj->reversed = 0;
+	obj->width = 0;
+	obj->height = 0;
+}
